e9429ebfb6b088a68f1fb363cd2074bb0191f3f2
Martial59110 <floquet.martial@gmail.com>
2023-12-28 13:41:41 +0100

    Feat : add contact

--- LaBonneAuberge/Controllers/HomeController.cs
+++ LaBonneAuberge/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace LaBonneAuberge.Controllers
         {
             return View();
         }
+          public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
