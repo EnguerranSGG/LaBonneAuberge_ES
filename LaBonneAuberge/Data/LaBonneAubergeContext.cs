50a4b86991afb96475467cc64f69e2eb23967aff
ACH90 <chassagne.alexandre@gmail.com>
2023-12-29 14:12:07 +0100

    Fix and Feat Menu db

---
+++ LaBonneAuberge/Data/LaBonneAubergeContext.cs
@@ -0,0 +1,15 @@
+using LaBonneAuberge.Models;
+using Microsoft.EntityFrameworkCore;
+namespace LaBonneAuberge.Data
+{
+ public class LaBonneAubergeContext : DbContext
+ {
+ public LaBonneAubergeContext(DbContextOptions<LaBonneAubergeContext> options) :
+base(options)
+ {
+ }
+  public DbSet<TeamList> TeamLists { get; set; }
+  public DbSet<Menu> Menus { get; set; } = default!;
+//  public DbSet<Category> Categories { get; set; }
+ }
+}
