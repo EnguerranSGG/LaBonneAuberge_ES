241d82e080c61acdfc69d282902f20096ea13b7e
Martial59110 <floquet.martial@gmail.com>
2023-12-29 12:25:30 +0100

    add DB TeamList

---
+++ LaBonneAuberge/Data/LaBonneAubergeContext.cs
@@ -0,0 +1,16 @@
+using LaBonneAuberge.Models;
+using Microsoft.EntityFrameworkCore;
+namespace LaBonneAuberge.Data
+{
+ public class LaBonneAubergeContext : DbContext
+ {
+ public LaBonneAubergeContext(DbContextOptions<LaBonneAubergeContext> options) :
+base(options)
+ {
+ }
+  public DbSet<TeamList> TeamLists { get; set; }
+//  public DbSet<Category> Categories { get; set; }
+ }
+}
+
+
--- LaBonneAuberge/Program.cs
+++ LaBonneAuberge/Program.cs
@@ -1,8 +1,13 @@
+using LaBonneAuberge.Data;
+using Microsoft.EntityFrameworkCore;
+using LaBonneAuberge.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<LaBonneAubergeContext>(options=>
+options.UseSqlite(builder.Configuration.GetConnectionString("LaBonneAubergeContext")?? throw new InvalidOperationException("Connection string 'TodoListContext' not found.")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
