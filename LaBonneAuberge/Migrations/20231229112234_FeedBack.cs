211ef6da9300d07071d40f5747e46b0c9c9a773d
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-01-02 11:25:02 +0100

    Changes name of migration files

--- LaBonneAuberge/Migrations/20231229112234_FeedBack.cs
+++ LaBonneAuberge/Migrations/20231229112234_FeedBack.cs
@@ -5,7 +5,7 @@
 namespace LaBonneAuberge.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class FeedBack : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
