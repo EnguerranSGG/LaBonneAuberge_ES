685e023a812086084c500c41f593a51430027632
ACH90 <chassagne.alexandre@gmail.com>
2024-01-03 16:04:55 +0100

    Create Category

--- LaBonneAuberge/Inserts.sql
+++ LaBonneAuberge/Inserts.sql
@@ -1,13 +1,12 @@
--- Inserts Menus
+-- Exemples d'insertions avec des ID différents
+INSERT INTO Menus ( Name, Description, Price, CategoryId)
+VALUES ('Filet Mignon', 'Un morceau tendre et juteux de filet mignon de bœuf, grillé à la perfection.', 35.99, 1);
 
-INSERT INTO Menus (Id, Name, Description, Price)
-VALUES (1, 'Filet Mignon', 'Un morceau tendre et juteux de filet mignon de bœuf, grillé à la perfection.', 35.99);
+INSERT INTO Menus (Name, Description, Price, CategoryId)
+VALUES ( 'Côte de Porc Glacée au Miel', 'Côtes de porc épaisses cuites lentement et glacées avec une sauce au miel délicieusement sucrée.', 28.50, 1);
 
-INSERT INTO Menus (Id, Name, Description, Price)
-VALUES (2, 'Côte de Porc Glacée au Miel', 'Côtes de porc épaisses cuites lentement et glacées avec une sauce au miel délicieusement sucrée.', 28.50);
+INSERT INTO Menus ( Name, Description, Price, CategoryId)
+VALUES ( 'Assiette Mixte Grillée', 'Une sélection de viandes grillées comprenant du bœuf, de l''agneau et du poulet, servie avec des légumes grillés.', 42.75, 1);
 
-INSERT INTO Menus (Id, Name, Description, Price)
-VALUES (3, 'Assiette Mixte Grillée', 'Une sélection de viandes grillées comprenant du bœuf, de l''agneau et du poulet, servie avec des légumes grillés.', 42.75);
-
-INSERT INTO Menus (Id, Name, Description, Price)
-VALUES (4, 'Steak au Poivre', 'Un steak savoureux recouvert d''une sauce au poivre noir, accompagné de pommes de terre rissolées.', 31.50);
+INSERT INTO Menus ( Name, Description, Price, CategoryId)
+VALUES ('Steak au Poivre', 'Un steak savoureux recouvert d''une sauce au poivre noir, accompagné de pommes de terre rissolées.', 31.50,1);
